f10c992e1110223d4653b69efc982b897b5b4da1
Gabriel Echeverria <gabrielecheverria@ravn.co>
2023-03-09 14:39:44 -0600

    Initial Commit

---
+++ ScreenCapture/ScreenCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCaptureApp.swift
+//  ScreenCapture
+//
+//  Created by Gabriel Echeverria on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
